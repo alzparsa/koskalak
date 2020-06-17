d99a6fc631c86a8f21de36d7b6fcf559ea776686
Milad Herfeh <miladherfeh@gmail.com>
2020-06-17 19:16:57 +0430

    added the GetRate_irr, cleaned the index, added comments

--- testflask.py
+++ testflask.py
@@ -4,7 +4,9 @@ import requests
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+
+#This function would get the btc exchange to usd and return it to the page
+def GetRate_btc():
     url = 'https://api.coingecko.com/api/v3/exchange_rates'
     r = requests.get(url)
     BTCValueCad = r.json()["rates"]["cad"]["value"]
@@ -12,7 +14,15 @@ def hello_world():
     return render_template('home.html', BTCValueCad = BTCValueCad)
 
 
+#functions under devlopment 
 
+#This would get the IRR rate from jafari.pw and return the exact sell string to be used for exchange
+#1. for now it works on refresh  TODO:make it sync every 1 minute + render it into the page 
+def GetRate_irr():
+    url = 'https://currency.jafari.pw/json'
+    r = requests.get(url)
+    Rate_irr = r.json()["Currency"][0]['Sell']
+    return (Rate_irr)
 
 
 if __name__ == '__main__':
